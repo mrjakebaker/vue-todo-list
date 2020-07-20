11fcc974857047e73fd633d5a17418f4932c9e1e
mrjakebaker <mrjakebaker@gmail.com>
2020-07-20 10:32:47 +0200

    updating padding on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,54 @@
 <template>
-  <div id="app">
-    <Header />
-    <AddTodo @add-todo="addTodo" />
-    <Todos v-bind:todos="todos" @del-todo="deleteTodo" />
-  </div>
+	<div id="app">
+		<Header />
+		<AddTodo @add-todo="addTodo" />
+		<Todos v-bind:todos="todos" @del-todo="deleteTodo" />
+	</div>
 </template>
 
 <script>
-import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import Todos from "./components/Todos";
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 export default {
-  name: "App",
-  components: {
-    Header,
-    AddTodo,
-    Todos
-  },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => {
-        return todo.id !== id;
-      });
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    }
-  },
-  created() {
-    fetch("https://jsonplaceholder.typicode.com/todos/?_limit=7")
-      .then(response => response.json())
-      .then(json => (this.todos = json));
-  }
+	name: 'App',
+	components: {
+		Header,
+		AddTodo,
+		Todos,
+	},
+	data() {
+		return {
+			todos: [],
+		};
+	},
+	methods: {
+		deleteTodo(id) {
+			this.todos = this.todos.filter((todo) => {
+				return todo.id !== id;
+			});
+		},
+		addTodo(newTodo) {
+			this.todos = [...this.todos, newTodo];
+		},
+	},
+	created() {
+		fetch('https://jsonplaceholder.typicode.com/todos/?_limit=7')
+			.then((response) => response.json())
+			.then((json) => (this.todos = json));
+	},
 };
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 body {
-  margin: 0;
-  padding: 0;
-  background-color: #a3bffa;
+	margin: 0;
+	padding: 0;
+	background-color: #a3bffa;
 }
 
 h1,
@@ -61,43 +61,53 @@ p,
 a,
 span,
 li {
-  padding: 0;
-  margin: 0;
+	padding: 0;
+	margin: 0;
 }
 
-input[type="submit"],
+input[type='submit'],
 .btn {
-  cursor: pointer;
-  border: 0;
-  padding: 0.25rem 0.5rem;
-  border-radius: 6px;
+	cursor: pointer;
+	border: 0;
+	padding: 0.25rem 0.5rem;
+	border-radius: 6px;
 }
 
 .visually-hidden {
-  position: absolute !important;
-  opacity: 0;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-  clip: rect(1px, 1px, 1px, 1px);
-  white-space: nowrap; /* added line */
+	position: absolute !important;
+	opacity: 0;
+	height: 1px;
+	width: 1px;
+	overflow: hidden;
+	clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+	clip: rect(1px, 1px, 1px, 1px);
+	white-space: nowrap; /* added line */
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 20px auto;
-  width: 100%;
-  max-width: 480px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	margin: 20px auto;
+	width: 100%;
+	max-width: 480px;
+	padding: 0 0.5rem;
 }
 
 .icon {
-  height: 18px;
-  width: 18px;
+	height: 18px;
+	width: 18px;
 }
 
-/* primary, secondary, accent, error, warning, info, success */
+/* 
+background: #a3bffb,
+primary: #5a67d8, 
+secondary , 
+accent,
+error: #f56565, 
+warning: , 
+info, 
+success: light: #9ae6b4, dark: #48bb78, 
+*/
 </style>
