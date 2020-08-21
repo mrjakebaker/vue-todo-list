88159d91f4c910547930e12811ddd7cfd19248d1
mrjakebaker <mrjakebaker@gmail.com>
2020-08-21 11:10:30 +0200

    replacing initial 'done' message with 'loading'

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,115 @@
 <template>
-	<div id="app">
-		<Header />
-		<AddTodo @add-todo="addTodo" />
-		<Todos v-bind:todos="todos" @del-todo="deleteTodo" />
-	</div>
+  <div id="app">
+    <Header />
+    <AddTodo @add-todo="addTodo" />
+    <div v-if="todos.length == 0" class="loading">Loading</div>
+    <Todos :todos="todos" :isLoading="isLoading" @del-todo="deleteTodo" v-else />
+  </div>
 </template>
 
 <script>
-import db from './components/firebaseInit';
-import moment from 'moment';
-import Header from './components/layout/Header';
-import AddTodo from './components/AddTodo';
-import Todos from './components/Todos';
+import db from "./components/firebaseInit";
+import moment from "moment";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 export default {
-	name: 'App',
-	components: {
-		Header,
-		AddTodo,
-		Todos,
-	},
-	data() {
-		return {
-			todos: [],
-			isLoading: true,
-		};
-	},
-	methods: {
-		deleteTodo(id) {
-			console.log(id);
-			db.collection('todos')
-				.where('todo_id', '==', id)
-				.get()
-				.then((querySnapshot) => {
-					querySnapshot.forEach((doc) => {
-						doc.ref
-							.delete()
-							.then(() => {
-								console.log('Document successfully deleted!');
-							})
-							.catch(function(error) {
-								console.error('Error removing document: ', error);
-							});
-					});
-				});
-			this.todos = this.todos.filter((todo) => {
-				return todo.todo_id !== id;
-			});
-		},
-		addTodo(newTodo) {
-			this.todos = [...this.todos, newTodo];
-		},
-	},
-	created() {
-		// fetch('https://jsonplaceholder.typicode.com/todos/?_limit=7')
-		// 	.then((response) => response.json())
-		// 	.then((json) => (this.todos = json))
-
-		db.collection('todos')
-			.get()
-			.then((querySnapshot) => {
-				querySnapshot.forEach((doc) => {
-					const data = {
-						id: doc.id,
-						todo_id: doc.data().todo_id,
-						title: doc.data().title,
-						completed: doc.data().completed,
-						due: moment(doc.data().due.seconds * 1000, 'x').format(
-							'dddd, MMMM Do YYYY'
-						),
-					};
-					this.todos.push(data);
-				});
-			})
-			.then(console.log((this.isLoading = false)));
-	},
+  name: "App",
+  components: {
+    Header,
+    AddTodo,
+    Todos,
+  },
+  data() {
+    return {
+      todos: [],
+      isLoading: true,
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      console.log(id);
+      db.collection("todos")
+        .where("todo_id", "==", id)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            doc.ref
+              .delete()
+              .then(() => {
+                console.log("Document successfully deleted!");
+              })
+              .catch(function (error) {
+                console.error("Error removing document: ", error);
+              });
+          });
+        });
+      this.todos = this.todos.filter((todo) => {
+        return todo.todo_id !== id;
+      });
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
+  created() {
+    // fetch('https://jsonplaceholder.typicode.com/todos/?_limit=7')
+    // 	.then((response) => response.json())
+    // 	.then((json) => (this.todos = json))
+    console.log(this.isLoading);
+    db.collection("todos")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const data = {
+            id: doc.id,
+            todo_id: doc.data().todo_id,
+            title: doc.data().title,
+            completed: doc.data().completed,
+            due: moment(doc.data().due.seconds * 1000, "x").format(
+              "dddd, MMMM Do YYYY"
+            ),
+          };
+          this.todos.push(data);
+        });
+      })
+      .then((this.isLoading = false));
+    console.log(this.isLoading);
+  },
 };
 </script>
 
 <style>
 * {
-	box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 :root {
-	--background: #a3bffb;
-	--surface: #5a67d8;
+  --background: #a3bffb;
+  --surface: #5a67d8;
 
-	--primary: #5a67d8;
-	--primary-alt: ;
-	--secondary: ;
-	--secondary-alt: ;
+  --primary: #5a67d8;
+  --primary-alt: ;
+  --secondary: ;
+  --secondary-alt: ;
 
-	--text: #3c366b;
-	--text-alt: #2c3e50;
-	--text-on-surface: #f7fafc;
+  --text: #3c366b;
+  --text-alt: #2c3e50;
+  --text-on-surface: #f7fafc;
 
-	--error: #f56565;
-	--error-active: ;
+  --error: #f56565;
+  --error-active: ;
 
-	--warning: #faf089;
-	--info: ;
-	--success: #9ae6b4;
-	--success-active: #48bb78;
+  --warning: #faf089;
+  --info: ;
+  --success: #9ae6b4;
+  --success-active: #48bb78;
 }
 
 body {
-	margin: 0;
-	padding: 0;
-	background-color: var(--background);
+  margin: 0;
+  padding: 0;
+  background-color: var(--background);
 }
 
 h1,
@@ -120,65 +122,69 @@ p,
 a,
 span,
 li {
-	padding: 0;
-	margin: 0;
+  padding: 0;
+  margin: 0;
 }
 
-input[type='submit'],
+input[type="submit"],
 .btn {
-	cursor: pointer;
-	border: 0;
-	padding: 0.25rem 0.5rem;
-	border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  padding: 0.25rem 0.5rem;
+  border-radius: 6px;
 }
 
 .btn--right {
-	margin-left: auto;
+  margin-left: auto;
 }
 
 .visually-hidden {
-	position: absolute !important;
-	opacity: 0;
-	height: 1px;
-	width: 1px;
-	overflow: hidden;
-	clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-	clip: rect(1px, 1px, 1px, 1px);
-	white-space: nowrap; /* added line */
+  position: absolute !important;
+  opacity: 0;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+  clip: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap; /* added line */
 }
 
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: var(--text);
-	margin: 20px auto;
-	width: 100%;
-	max-width: 480px;
-	padding: 0 0.5rem;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: var(--text);
+  margin: 20px auto;
+  width: 100%;
+  max-width: 480px;
+  padding: 0 0.5rem;
 }
 
 .icon {
-	height: 18px;
-	width: 18px;
+  height: 18px;
+  width: 18px;
 }
 
-input[type='color'],
-input[type='date'],
-input[type='datetime'],
-input[type='datetime-local'],
-input[type='email'],
-input[type='month'],
-input[type='number'],
-input[type='password'],
-input[type='search'],
-input[type='tel'],
-input[type='text'],
-input[type='time'],
-input[type='url'],
-input[type='week'],
+input[type="color"],
+input[type="date"],
+input[type="datetime"],
+input[type="datetime-local"],
+input[type="email"],
+input[type="month"],
+input[type="number"],
+input[type="password"],
+input[type="search"],
+input[type="tel"],
+input[type="text"],
+input[type="time"],
+input[type="url"],
+input[type="week"],
 select:focus,
 textarea {
-	font-size: 16px;
+  font-size: 16px;
+}
+
+.loading {
+  text-align: center;
 }
 </style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,83 +1,77 @@
 <template>
-	<div class="todo__list">
-		<transition name="modal">
-			<Modal
-				v-if="showModal"
-				@close="showModal = false"
-				:activeModal="activeModal"
-				:todos="todos"
-			/>
-		</transition>
-		<div v-if="activeTodo.length > 0" class="todo__list--populated">
-			<div v-bind:key="todo.id" v-for="todo in activeTodo">
-				<TodoItem
-					v-bind:todo="todo"
-					@del-todo="$emit('del-todo', todo.todo_id)"
-					@open-modal="openModal"
-				/>
-			</div>
-		</div>
-		<div class="todo__list--empty" v-else>
-			<img src="../assets/complete.svg" height="50" alt="" />
-			<h2>
-				All Done
-			</h2>
-		</div>
-		<h2 class="todo__list-title">Completed</h2>
-		<div v-bind:key="todo.id" v-for="todo in completedTodo">
-			<TodoItem v-bind:todo="todo" @del-todo="$emit('del-todo', todo.id)" />
-		</div>
-	</div>
+  <div class="todo__list">
+    <transition name="modal">
+      <Modal v-if="showModal" @close="showModal = false" :activeModal="activeModal" :todos="todos" />
+    </transition>
+
+    <div v-if="activeTodo.length == 0 && todos.length > 0" class="todo__list--empty">
+      <img src="../assets/complete.svg" height="50" alt />
+      <h2>All Done</h2>
+    </div>
+    <div class="todo__list--populated" v-else>
+      <div v-bind:key="todo.id" v-for="todo in activeTodo">
+        <TodoItem
+          v-bind:todo="todo"
+          @del-todo="$emit('del-todo', todo.todo_id)"
+          @open-modal="openModal"
+        />
+      </div>
+    </div>
+    <h2 class="todo__list-title">Completed</h2>
+    <div v-bind:key="todo.id" v-for="todo in completedTodo">
+      <TodoItem v-bind:todo="todo" @del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue';
-import Modal from './Modal.vue';
+import TodoItem from "./TodoItem.vue";
+import Modal from "./Modal.vue";
 export default {
-	name: 'Todos',
-	components: {
-		TodoItem,
-		Modal,
-	},
-	data() {
-		return {
-			showModal: false,
-			activeModal: -1,
-		};
-	},
-	props: ['todos'],
-	methods: {
-		openModal(id) {
-			// console.log(id);
-			this.activeModal = id;
-			this.showModal = true;
-		},
-	},
-	computed: {
-		reverseTodos() {
-			return this.todos.slice().reverse();
-		},
-		activeTodo() {
-			let activeTodos = this.todos.filter((todo) => {
-				return todo.completed !== true;
-			});
+  name: "Todos",
+  components: {
+    TodoItem,
+    Modal,
+  },
+  data() {
+    return {
+      showModal: false,
+      activeModal: -1,
+    };
+  },
+  props: ["todos", "isLoading"],
+  methods: {
+    openModal(id) {
+      // console.log(id);
+      this.activeModal = id;
+      this.showModal = true;
+    },
+  },
+  computed: {
+    reverseTodos() {
+      return this.todos.slice().reverse();
+    },
+    activeTodo() {
+      let activeTodos = this.todos.filter((todo) => {
+        return todo.completed !== true;
+      });
 
-			return activeTodos.slice().reverse();
-		},
-		completedTodo() {
-			let completedTodos = this.todos.filter((todo) => {
-				return todo.completed == true;
-			});
+      return activeTodos.slice().reverse();
+    },
+    completedTodo() {
+      let completedTodos = this.todos.filter((todo) => {
+        return todo.completed == true;
+      });
 
-			return completedTodos.slice().reverse();
-		},
-	},
+      return completedTodos.slice().reverse();
+    },
+  },
 };
 </script>
 
 <style scoped>
 .todo__list-title {
-	margin: 1.5rem 0 1rem;
-	font-size: 16px;
+  margin: 1.5rem 0 1rem;
+  font-size: 16px;
 }
 </style>
